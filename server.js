1e29d45dc67b123994f09f9f6af3222334243215
Alxandr <alxandr@alxandr.me>
2015-01-27 21:43:22 +0100

    Looking like something

--- server.js
+++ server.js
@@ -1,22 +1,65 @@
-var connect = require('connect'),
-    static = require('serve-static'),
-    http = require('http'),
-    path = require('path');
+var path = require('path'),
+    fs = require('fs');
 
-console.log('imported');
+function staticFile(p, mime) {
+  return function(req, res, next) {
+    if(req.url.substring(1) === p) {
+      returnFile(req, res, path.join(__dirname, p), mime);
+      return;
+    }
+
+    next();
+  };
+}
+
+function returnFile(req, res, file, mime) {
+  fs.stat(file, function(err, stat) {
+    if(err) {
+      res.write(err);
+      res.end();
+      return;
+    }
+
+    res.writeHead(200, {
+      'Content-Type': mime,
+      'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(file);
+    readStream.pipe(res);
+  });
+}
 
 module.exports = {
   startServer: function(config, callback) {
+    var connect = require('connect'),
+        static = require('serve-static'),
+        compression = require('compression'),
+        http = require('http');
+
+
     var app = connect();
     var port = process.env.PORT || config.server.port || 3000;
 
 
     // gzip/deflate outgoing responses
-    var compression = require('compression');
     app.use(compression());
     app.use('/lib', static('lib'));
+    app.use('/jspm_packages', static('jspm_packages'));
+    app.use(staticFile('config.js', 'text/javascript'));
+    app.use(function(req, res, next) {
+      if (req.url.indexOf('/lib') === 0) {
+        res.writeHead(404, 'Not Found');
+        res.end();
+        return;
+      }
+
+      next();
+    });
+
+    var indexFile = path.join(__dirname, 'index.html');
     app.use(function(req, res) {
-      res.sendFile(path.join(__dirname, 'index.html'))
+      returnFile(req, res, indexFile, 'text/html');
     });
 
 

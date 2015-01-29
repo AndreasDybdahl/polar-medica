ead095d67fcd87f0d7f173b4330be813a320516d
Alxandr <alxandr@alxandr.me>
2015-01-29 14:19:07 +0100

    bundleing

--- src/app/main.js
+++ src/app/main.js
@@ -1,13 +1,27 @@
 import 'bootstrap/css/bootstrap.css!';
 import 'style/site.css!'
 
+/* The following imports are only to include the frameworks when bundeling */
+import 'aurelia-templating-binding';
+import 'aurelia-templating-router';
+import 'aurelia-templating-resources';
+import 'aurelia-event-aggregator';
+import 'aurelia-router';
+import 'aurelia-history';
+import 'aurelia-history-browser';
+
+import 'moment';
+import 'showdown';
+/* End of bundle only imports */
+
 import {LogManager} from 'aurelia-framework';
 import {ConsoleAppender} from 'aurelia-logging-console';
+import {bootstrap} from 'aurelia-bootstrapper';
 
 LogManager.addAppender(new ConsoleAppender());
 LogManager.setLevel(LogManager.levels.debug);
 
-export function configure(aurelia) {
+bootstrap(aurelia => {
   aurelia.use
     .defaultBindingLanguage()
     .defaultResources()
@@ -15,4 +29,4 @@ export function configure(aurelia) {
     .eventAggregator();
 
   aurelia.start().then(a => a.setRoot('lib/app/routes/app', document.body));
-}
+});

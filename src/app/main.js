ba886a01533622dd87542fa835b249e317a1fe0c
AndreasDybdahl <andreas.dyb@gmail.com>
2015-09-22 16:15:43 +0200

    Changed CSS path

--- src/app/main.js
+++ src/app/main.js
@@ -1,4 +1,4 @@
-import './bootstrap-sandstone.min.css!';
+import 'lib/app/bootstrap-sandstone.min.css!';
 import 'font-awesome/css/font-awesome.css!';
 import 'style/site.css!'
 
@@ -9,13 +9,13 @@ bootstrap(aurelia => {
   aurelia.use
     .standardConfiguration()
     .developmentLogging();
-  
+
   let container = aurelia.container;
   let auth = new AuthService();
-    
+
   container.registerInstance(AuthService, auth);
   auth.http = container.get(HttpClient);
-  
+
   auth.attemptRelogin().then(() => {
     return aurelia.start().then(a => a.setRoot('app/app', document.body));
   });
